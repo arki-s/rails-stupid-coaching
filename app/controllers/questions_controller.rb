7409a079b826951691f330756691731855c3150c
exec zsh <wind10cloud@gmail.com>
2023-05-08 15:57:13 +0900

    added

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,22 @@
+class QuestionsController < ApplicationController
+
+  def home
+  end
+
+  def ask
+    if @question
+       redirect_to '/answer'
+    end
+  end
+
+  def answer
+    @question = params[:question]
+    if @question == "I am going to work"
+      @answer = "Great!"
+    elsif @question.include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
